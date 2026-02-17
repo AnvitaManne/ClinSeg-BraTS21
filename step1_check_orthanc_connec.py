a53407fec25b800248f7c6c8cb45b66f4692eee9
AnvitaManne <anvita.manne@gmail.com>
2026-02-17 12:11:19 +0530

    Update

--- step1_check_orthanc_connec.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-ORTHANC_URL = "http://localhost:8042"
-AUTH = ("orthanc", "orthanc")  # change if needed
-
-r = requests.get(f"{ORTHANC_URL}/studies", auth=AUTH)
-
-print("Status:", r.status_code)
-print("Studies:", r.json())
